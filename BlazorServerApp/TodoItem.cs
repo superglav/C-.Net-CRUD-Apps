c80f66bb12553fa6fee1b6e6d2b5316ed0ec44b8
Nikos Glavinas <nickosglavinas@gmail.com>
2022-10-11 14:30:07 +0300

    Delete BlazorServerApp directory

--- BlazorServerApp/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorServerApp
-{
-    public class TodoItem
-    {
-        public int Id { get; set; }
-        public String? Title { get; set; }
-        public bool IsDone { get; set; }
-    }
-}
