6060b46bd38fece3f2ef2a47f989062356a1a10c
Nick Glavinas <nickosglavinas@gmail.com>
2022-10-16 14:31:29 +0300

    new Crud App with EF and MVC

---
+++ MVCCrudEfApp/MVCCrudEfApp/Data/MvcDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCCrudEfApp.Models.Domain;
+
+namespace MVCCrudEfApp.Models.Data
+{
+    public class MvcDemoDbContext : DbContext
+    {
+        public MvcDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ MVCCrudEfApp/MVCCrudEfApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCCrudEfApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
